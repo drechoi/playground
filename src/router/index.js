2fccfa018148fafa26ef4fee003efb1d62041832
andre <andre.cfh@gmail.com>
2020-09-30 00:02:17 +0800

    created few dummy pages and setup router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Landing from '@/views/Landing'
+import Content from '@/views/Content'
+import Error from '@/views/Error'
 
 Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    name: 'Landing',
+    component: Landing
+  },
+  {
+    path: '/content',
+    name: 'Content',
+    component: Content
+  },
+  {
+    path: '/*',
+    name: 'error',
+    component: Error
+  }
+]
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  mode: 'history',
+  routes
 })
---
+++ src/views/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>This is landing page</h1>
+    <img src="../assets/logo.png" class="tmp">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.tmp {
+  width: 30em
+}
+</style>
