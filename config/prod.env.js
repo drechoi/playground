b0bf989fa8b0dcc30c10d4a399cfe08118b4ab37
andre <andre.cfh@gmail.com>
2020-10-06 01:08:13 +0800

    env.js update again

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  NODE_PUBLIC_PATH: process.env.publicPath,
-  NODE_BASE_URL: process.env.BASE_URL
+  NODE_PUBLIC_PATH: 'process.env.publicPath',
+  NODE_BASE_URL: 'process.env.BASE_URL'
 }
