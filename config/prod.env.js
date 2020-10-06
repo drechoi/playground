617d4ec26189684a9f48fa218736529bdfe6d470
andre <andre.cfh@gmail.com>
2020-10-06 12:43:59 +0800

    router env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,10 @@
 'use strict'
+
+console.log('prod env js')
+console.log(process.env)
+
 module.exports = {
   NODE_ENV: '"production"',
-  NODE_PUBLIC_PATH: 'process.env.publicPath',
-  NODE_BASE_URL: 'process.env.BASE_URL'
+  NODE_PUBLIC_PATH: process.env.publicPath,
+  NODE_BASE_URL: process.env.dummy
 }
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,7 +3,9 @@
     <h1>This is Playground</h1>
     <img src="../assets/logo.png" class="tmp">
     <br />
+    <router-link to="/content">Table of content</router-link>
     <a href="/content">Table of content</a>
+    <router-link to="/login">Login page</router-link>
     <a href="/login">Login page</a>
   </div>
 </template>
